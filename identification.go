6c1f50c099e37c85d0e930cfaea49e2d33baf104
odwnlaptop <odwnisaperson@gmail.com>
2024-09-12 20:25:00 +1000

    Update formats switch

--- identification.go
+++ identification.go
@@ -12,7 +12,7 @@ type FileIdentification struct {
 func Identify(name string) FileIdentification {
 	name = strings.ToLower(name)
 	var extension string = strings.Split(name, ".")[len(strings.Split(name, "."))-1]
-	var id FileIdentification = FileIdentification{Ansi("White"), "\uf15b", "File"}
+	var id FileIdentification = FileIdentification{Ansi("White"), "    ", "File"}
 	switch extension {
 		case "go":
 			id = FileIdentification{Ansi("Cyan"), "=>Go", "Go"}
@@ -27,7 +27,7 @@ func Identify(name string) FileIdentification {
 				id = FileIdentification{Ansi("LightCyan"), "=>Go", "Go Checksum"}
 			}
 		case "js":
-			id = FileIdentification{Ansi("Yellow"), "[JS]", "Javascript"}
+			id = FileIdentification{Ansi("LightYellow"), "[JS]", "Javascript"}
 		case "html", "htm", "xhtml":
 			id = FileIdentification{Ansi("LightRed"), "{5} ", "HTML"}
 		case "css", "scss", "sass":
@@ -38,6 +38,30 @@ func Identify(name string) FileIdentification {
 			} else {
 				id = FileIdentification{Ansi("Cyan"), "<M↓>", "Markdown"}
 			}
+		case "bat", "cmd", "sh":
+			id = FileIdentification{Ansi("LightGreen"), "[>_]", "Command-Line Script"}
+		case "c":
+			id = FileIdentification{Ansi("LightBlue"), "(C) ", "C"}
+		case "h", "hpp", "hh":
+			id = FileIdentification{Ansi("Cyan"), "(h) ", "C Header File"}
+		case "cpp", "cc":
+			id = FileIdentification{Ansi("LightBlue"), "*C++", "C++"}
+		case "json":
+			id = FileIdentification{Ansi("Yellow"), "{;} ", "JSON"}
+		case "py", "pyc", "ipynb":
+			id = FileIdentification{Ansi("LightBlue"), "&"+Ansi("Cyan")+"p"+Ansi("LightYellow")+"y"+Ansi("Yellow")+">", "Python"}
+		case "txt", "text":
+			id = FileIdentification{Ansi("White"), "... ", "Raw Text File"}
+		case "rb":
+			id = FileIdentification{Ansi("Red"), "/rb>", "Ruby"}
+		case "log":
+			id = FileIdentification{Ansi("White"), "LOG"+Ansi("Red")+"!", "Log File"}
+		case "lua":
+			id = FileIdentification{Ansi("Blue"), "oO()", "Lua Script"}
+		case "nvim":
+			id = FileIdentification{Ansi("LightBlue"), "|"+Ansi("LightGreen")+"\\"+Ansi("Green")+"| ", "Neovim Config"}
+		case "gz", "zip", "7z", "rar":
+			id = FileIdentification{Ansi("Red"), "0"+Ansi("Green")+"0"+Ansi("LightBlue")+"0 ", "Compressed Archive"}
 	}
 
 	return id
